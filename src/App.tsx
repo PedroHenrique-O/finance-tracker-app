5000b4f90acdca13156612d03977eea735c15311
Pedro <pedrokaz.henrique@gmail.com>
2021-11-06 23:23:43 -0300

    commit1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import * as C from "./App.styles";
+import { Categories } from "./types/Categories";
+import { categories } from "./data/categories";
+import { Item } from "./types/Item";
+import { items } from "./data/items";
+import { GetCurrentMonth } from "./helpers/dateFilters";
 
-function App() {
+const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(GetCurrentMonth());
+
+  useEffect(() => {}, [list, currentMonth]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/* {Área de informações} */}
+        {/* {Área de inserção de informação} */}
+        {/* {tabela de items} */}
+      </C.Body>
+    </C.Container>
   );
-}
-
+};
 export default App;
---
+++ src/data/items.ts
@@ -0,0 +1,27 @@
+import { Item } from "../types/Item";
+export const items: Item[] = [
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "MacDonalds",
+    value: 32.12,
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "BuguerKing",
+    value: 32.12,
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "Pizza",
+    value: 32.12,
+  },
+  {
+    date: new Date(2021, 10, 18),
+    category: "rend",
+    title: "Aluguel Apt",
+    value: 2300,
+  },
+];
